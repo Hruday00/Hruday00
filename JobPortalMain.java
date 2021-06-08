57e2059301fa6c7bc577271ad858435919a1a10f
Hruday00 <66170675+Hruday00@users.noreply.github.com>
2021-06-08 22:00:06 +0530

    Add files via upload

---
+++ JobPortalMain.java
@@ -0,0 +1,14 @@
+package org.project.jobportal;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+public class JobPortalMain {
+public static void main(String[] args) {
+HomePage.init();
+	}
+}
